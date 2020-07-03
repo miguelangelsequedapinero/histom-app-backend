75f0581e49119564c7a9248c90f3908593ea400e
Stephan Schmidt <stephan@hivomedia.de>
2020-07-03 16:38:53 +0200

    Added Database-connection

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,12 @@
-var express = require('express');
-var router = express.Router();
-const jsonData = require('./histom_fake_data_v0.0.json');
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controllers/index-controller');
+
 
 /* GET single id data with complete data from json-file. */
-router.get('/:id', function (req, res, next) {
-  const idAsNumber = parseInt(req.params.id,10);
-
-  let sendJsonData;
-  if (jsonData.length >= idAsNumber){
-    sendJsonData = jsonData.filter((data) => {
-      return data.id === idAsNumber;
-    });
-  }else{
-    sendJsonData = {
-      error: 'no data for this id / route'
-    }
-  }
-
-  res.send(sendJsonData);
-});
+router.get('/:id', indexController.getGenreListItemById );
 
 /* GET root data with id, name, short-description and decade. */
-router.get('/', function (req, res, next) {
-
-  const sendJsonData = [];
-  jsonData.map((data) => {
-    const shortDescription = data.description.slice(0, 100) + ' ...';
-    const dataObj = {
-      id: data.id,
-      name: data.name,
-      description: shortDescription,
-      decade: data.decade
-    }
-    sendJsonData.push(dataObj);
-  });
-
-  res.send(sendJsonData);
-});
+router.get('/', indexController.getGenreList );
 
 module.exports = router;
